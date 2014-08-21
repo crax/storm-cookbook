6bd73b1fb12ca2c58289880d50900095151ef1e1
dcouture <dcouture@dyn.com>
2014-08-21 15:33:45 -0400

    Adding storm user and switching to /etc as default location

--- attributes/default.rb
+++ attributes/default.rb
@@ -7,8 +7,8 @@
 # All rights reserved - Do Not Redistribute
 #
 
-default[:storm][:deploy][:user] = "root"
-default[:storm][:deploy][:group] = "root"
+default[:storm][:deploy][:user] = "storm"
+default[:storm][:deploy][:group] = "storm"
 
 default[:storm][:nimbus][:host] = "localhost"
 default[:storm][:supervisor][:hosts] = [ "localhost" ]
@@ -23,7 +23,7 @@ default[:storm][:ui][:childopts] = "-Xmx512m -Djava.net.preferIPv4Stack=true"
 
 default[:storm][:version] = "0.9.1-incubating"
 default[:storm][:packages] = %w{ curl unzip build-essential pkg-config libtool autoconf git-core uuid-dev python-dev zookeeper }
-default[:storm][:path][:root] = "/opt/storm"
+default[:storm][:path][:root] = "/etc/storm"
 
 ## Dependent on resolved node attributes.  Override these as well if
 ## another version is required in a wrapper cookbook

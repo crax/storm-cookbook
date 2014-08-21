951735e66b4fdf307706e13a65cc553a0ea956c0
dcouture <dcouture@dyn.com>
2014-08-21 15:35:24 -0400

    Creating storm user/group, running notifies immediately, adding /etc/default/storm env variables

--- recipes/default.rb
+++ recipes/default.rb
@@ -7,6 +7,14 @@
 # All rights reserved - Do Not Redistribute
 #
 
+group node[:storm][:deploy][:group]
+user node[:storm][:deploy][:user] do
+  gid node[:storm][:deploy][:group]
+  comment "storm user"
+  system true
+  shell "/bin/false"
+end
+
 node[:storm][:packages].each do |pkg|
   package pkg do
     retries 2
@@ -23,15 +31,21 @@ local_file = ::File.join(Chef::Config[:file_cache_path],
 remote_file local_file do
   source ::File.join(node[:storm][:remote_path], node[:storm][:remote_file])
   action :create_if_missing
-  notifies :run, "execute[extract_storm]"
-  not_if { ::File.exists?(node[:storm][:path][:version])}
+  notifies :run, "execute[extract_storm]", :immediately
+  not_if { ::File.exists?(node[:storm][:path][:version]) }
 end
 
 execute "extract_storm" do
   command "unzip #{local_file} -d #{node[:storm][:path][:root]}"
   action :nothing
+  notifies :create, "link[#{::File.join(node[:storm][:path][:root], "current")}]", :immediately
 end
 
 link ::File.join(node[:storm][:path][:root], "current") do
   to node[:storm][:path][:version]
+end
+
+template "/etc/default/storm" do
+  source "env/storm.erb"
+  mode "0664"
 end

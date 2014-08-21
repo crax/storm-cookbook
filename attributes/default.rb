a60d21c42ad447ce2e2b5f853aa9095a74fc85d5
dcouture <dcouture@dyn.com>
2014-08-21 13:08:59 -0400

    Move install parameters to attributes

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,5 +1,14 @@
-default[:storm][:deploy][:user] = ::File.exists?("/home/vagrant") ? "vagrant" : "root"
-default[:storm][:deploy][:group] = ::File.exists?("/home/vagrant") ? "vagrant" : "root"
+#
+# Cookbook Name:: storm-project
+# Attribute:: default
+#
+# Copyright 2012, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+
+default[:storm][:deploy][:user] = "root"
+default[:storm][:deploy][:group] = "root"
 
 default[:storm][:nimbus][:host] = "localhost"
 default[:storm][:supervisor][:hosts] = [ "localhost" ]
@@ -13,7 +22,14 @@ default[:storm][:worker][:childopts] = "-Xmx512m -Djava.net.preferIPv4Stack=true
 default[:storm][:ui][:childopts] = "-Xmx512m -Djava.net.preferIPv4Stack=true"
 
 default[:storm][:version] = "0.9.1-incubating"
+default[:storm][:packages] = %w{ curl unzip build-essential pkg-config libtool autoconf git-core uuid-dev python-dev zookeeper }
+default[:storm][:path][:root] = "/opt/storm"
 
-default[:storm][:path][:user] = "/#{node[:storm][:deploy][:user]}"
-default[:storm][:path][:storm] = "#{default[:storm][:path][:user]}/apache-storm-#{node[:storm][:version]}"
+## Dependent on resolved node attributes.  Override these as well if
+## another version is required in a wrapper cookbook
+default[:storm][:remote_path] = ::File.join("http://apache.mirror.iweb.ca/incubator/storm/",
+                                            "apache-storm-#{node[:storm][:version]}")
+default[:storm][:remote_file] = "apache-storm-#{node[:storm][:version]}.zip"
 
+default[:storm][:path][:version] = ::File.join(node[:storm][:path][:root],
+                                               "apache-storm-#{node[:storm][:version]}")
